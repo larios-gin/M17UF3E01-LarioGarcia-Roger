b0cbfc8f33fd1b0655fb4d1085909bc9838898de
RogerLario <roger.lario.7e3@itb.cat>
2021-02-03 18:32:19 +0100

    Entrega Guitar Hero Game
    Entrega de la pràctica

---
+++ M17UF3E01-LarioGarcia-Roger/Assets/Scripts/Buttons.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour
+{
+    public KeyCode key;
+    bool isActive = false;
+    GameObject note;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(key) && isActive)
+        {
+            Destroy(note);
+            isActive = false;
+
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        isActive = true;
+        if (collision.gameObject.tag == "Note")
+        {
+            note = collision.gameObject;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        isActive = false;
+    }
+
+
+    public void sumarPuntuacion()
+    {
+        PlayerPrefs.SetInt("Puntuacion", PlayerPrefs.GetInt("Puntuacion") + 100);
+    }
+}
